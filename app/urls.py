e4b3d0fd404b5e21e418676580c9bdac5d9aca6c
ichiro kikuchi <65474842+kikusan-16@users.noreply.github.com>
2020-10-07 14:58:36 +0000

    init message

---
+++ app/urls.py
@@ -0,0 +1,30 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+from django.views.generic import TemplateView
+
+app_name = 'app'
+urlpatterns = [
+    path('', views.IndexView.as_view()),
+    path('articles/', views.ArticleListView.as_view(), name='article_list'),
+    path('articles/<int:pk>/', views.ArticleDetailView.as_view(), name='detail'),
+    #path('categories/', views.CategoryListView.as_view()),
+    path('categories/<str:category_slug>/', views.CategoryArticleView.as_view(), name='category_article'),
+    #path('tags/', views.TagListView.as_view()),
+    path('tags/<str:tag_slug>/', views.TagArticleView.as_view(), name='tag_article'),
+    path('search/', views.SearchArticleView.as_view(), name='search_article'),
+]
