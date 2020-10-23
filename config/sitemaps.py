b566af7c6cb3d3d6ed2ac640f304230f9c6494fb
kikusan-16 <67454842+kikusan-16@users.noreply.github.com>
2020-10-23 20:14:00 +0900

    sitemap created

--- config/sitemaps.py
+++ config/sitemaps.py
@@ -1,7 +1,7 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
-from app.models import Article
+from app.models import Article, Category, Tag
 
 class BlogArticleSitemap(Sitemap):
     changefreq = "never"
@@ -15,3 +15,36 @@ class BlogArticleSitemap(Sitemap):
     
     def lastmod(self, obj):
         return obj.published_at
+
+
+class BlogCategorySitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.8
+
+    def items(self):
+        return Category.objects.all()
+
+    def location(self, obj):
+        return reverse('app:category_article', args=[obj.category_slug])
+
+
+class BlogTagSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.8
+
+    def items(self):
+        return Tag.objects.all()
+
+    def location(self, obj):
+        return reverse('app:tag_article', args=[obj.tag_slug])
+
+
+class StaticSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.8
+
+    def items(self):
+        return ['app:index','app:article_list']
+
+    def location(self, item):
+        return reverse(item)
